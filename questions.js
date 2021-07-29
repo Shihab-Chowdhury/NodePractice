8bdc33c8c9e63fc2f99ba269d1f81ccda88df6dc
Shihab Chy <cshihab04@gmail.com>
2021-07-29 18:57:19 +0600

    Updated async process in questions

--- questions.js
+++ questions.js
@@ -1,3 +1,4 @@
+const { POINT_CONVERSION_COMPRESSED } = require("constants");
 const { ADDRGETNETWORKPARAMS } = require("dns");
 
 const questions = [
@@ -12,3 +13,8 @@ const ask = (i = 0) => {
 };
 
 ask();
+
+process.stdin.on ("data", data => {
+    process.stdout.write(`\n\n ${data.toString().trim()} \n\n`);
+    process.exit();
+});
